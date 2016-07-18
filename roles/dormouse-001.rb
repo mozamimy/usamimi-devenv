e9aebb3504759ee6cce771a9775d7d844525ace7
mozamimy (Moza USANE) <mozamimy@quellencode.org>
2016-07-18 17:44:52 +0900

    Install vim

--- roles/dormouse-001.rb
+++ roles/dormouse-001.rb
@@ -1,3 +1,4 @@
 include_recipe '../cookbooks/base/default.rb'
 include_recipe '../cookbooks/users/default.rb'
 include_recipe '../cookbooks/dotfiles/default.rb'
+include_recipe '../cookbooks/vim/default.rb'
